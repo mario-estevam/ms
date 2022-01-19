1678a0dfb986e3b7b25e78967873a4ac641f7c05
mario-estevam <mario.neto47@gmail.com>
2022-01-19 17:32:59 -0300

    Randon port to hr-wrok

--- hr-worker/src/main/java/com/mariobr/microservices/MicroservicesApplication.java
+++ hr-worker/src/main/java/com/mariobr/microservices/MicroservicesApplication.java
@@ -2,7 +2,9 @@ package com.mariobr.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicesApplication {
 
