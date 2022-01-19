96da32bcf0bd98e0e023bdd34663351c0391d795
mario-estevam <mario.neto47@gmail.com>
2022-01-19 16:50:27 -0300

    Project hr-eureka server created

---
+++ hr-eureka/src/test/java/com/mariobr/hreureka/HrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariobr.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
