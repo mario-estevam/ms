a560649b4635e2e56c6e7610e8a6d9970c5f0961
mario-estevam <mario.neto47@gmail.com>
2022-01-19 16:46:47 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/mariobr/microservices/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/mariobr/microservices/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.mariobr.microservices.resources;
 
 import com.mariobr.microservices.entities.Worker;
 import com.mariobr.microservices.repositories.WorkerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private WorkerRepository repository;
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -27,6 +38,7 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
