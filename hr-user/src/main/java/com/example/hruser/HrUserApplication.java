cc95c29c3dffb58e2be7f890fe99f3568ff4e8b1
mario-estevam <mario.neto47@gmail.com>
2022-01-19 22:54:58 -0300

    Database seeding

--- hr-user/src/main/java/com/example/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/example/hruser/HrUserApplication.java
@@ -1,13 +1,24 @@
 package com.example.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+    @Autowired
+    private BCryptPasswordEncoder pass;
 
     public static void main(String[] args) {
         SpringApplication.run(HrUserApplication.class, args);
+    }
+
+    @Override
+    public void run(String...args) throws Exception{
+        //System.out.println("BCRYPT = " + pass.encode("12445"));//macete pra gerar o BCrypt
     }
 
 }
